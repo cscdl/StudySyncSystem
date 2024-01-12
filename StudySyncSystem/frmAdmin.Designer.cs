f7ad9c7c39367c9732c30f4da359f95ad05db679
Cherish Marie Delos Santos <delossantos.169100@balagtas.sti.edu.ph>
2024-01-13 05:28:25 +0800

    ????????

--- StudySyncSystem/frmAdmin.Designer.cs
+++ StudySyncSystem/frmAdmin.Designer.cs
@@ -32,6 +32,7 @@ namespace StudySyncSystem
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdmin));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
             this.adminName = new System.Windows.Forms.Label();
             this.btnPicUser = new System.Windows.Forms.PictureBox();
             this.picBoxDashboard = new System.Windows.Forms.PictureBox();
@@ -59,8 +60,8 @@ namespace StudySyncSystem
             this.btnLogout = new System.Windows.Forms.Button();
             this.pnlMain = new System.Windows.Forms.Panel();
             this.tmrSideBar = new System.Windows.Forms.Timer(this.components);
-            this.panel10 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.panel10.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnPicUser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxDashboard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -77,7 +78,6 @@ namespace StudySyncSystem
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            this.panel10.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -92,12 +92,20 @@ namespace StudySyncSystem
             this.panel1.Size = new System.Drawing.Size(885, 50);
             this.panel1.TabIndex = 35;
             // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.adminName);
+            this.panel10.Location = new System.Drawing.Point(657, 11);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(190, 32);
+            this.panel10.TabIndex = 45;
+            // 
             // adminName
             // 
             this.adminName.AutoSize = true;
             this.adminName.BackColor = System.Drawing.Color.Transparent;
             this.adminName.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.adminName.Location = new System.Drawing.Point(116, 6);
+            this.adminName.Location = new System.Drawing.Point(116, 7);
             this.adminName.Name = "adminName";
             this.adminName.Size = new System.Drawing.Size(13, 18);
             this.adminName.TabIndex = 44;
@@ -372,14 +380,6 @@ namespace StudySyncSystem
             this.tmrSideBar.Interval = 20;
             this.tmrSideBar.Tick += new System.EventHandler(this.tmrSideBar_Tick);
             // 
-            // panel10
-            // 
-            this.panel10.Controls.Add(this.adminName);
-            this.panel10.Location = new System.Drawing.Point(657, 11);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(190, 32);
-            this.panel10.TabIndex = 45;
-            // 
             // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -393,6 +393,8 @@ namespace StudySyncSystem
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAdmin";
             this.panel1.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnPicUser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxDashboard)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -409,8 +411,6 @@ namespace StudySyncSystem
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            this.panel10.ResumeLayout(false);
-            this.panel10.PerformLayout();
             this.ResumeLayout(false);
 
         }
